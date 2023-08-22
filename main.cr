afcb0da82dfed5885d727aee46366f810ba5c874
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-22 09:30:08 +0600

    Add files via upload

---
+++ main.cr
@@ -0,0 +1,10 @@
+# This is a comment in Crystal. It's ignored by the Crystal compiler and used for documentation.
+
+# Define a method that takes a name and prints a greeting
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+# Call the greet method with different names
+greet("Alice")
+greet("Bob")
